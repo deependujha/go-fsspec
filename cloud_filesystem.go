cccf8d30865ba16217288b68a04b35b412856dfc
deependu <deependujha21@gmail.com>
2024-09-19 15:43:29 +0530

    repository structured and started working

---
+++ cloud_filesystem.go
@@ -0,0 +1,5 @@
+package go_fsspec
+
+type AbstractCloudFileStorage interface {
+	
+}
---
+++ constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	s3   = "s3"
+	gcs  = "gcs"
+	abfs = "abfs"
+)
+
+var SUPPORTED_CLOUD_PROVIDERS = [...]string{s3, gcs, abfs}
